ff0afdb41d28de19c0f05a87d097181f979ae7c3
DiamondDat <diamond.dat@1pac.vn>
2017-04-19 16:52:58 +0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
       get 'order_details'
     end
   end
-  resources  :categories,  only: [:index, :create, :update, :destroy] do
+  resources  :categories do
     resources :categories, only: [:show, :update, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
