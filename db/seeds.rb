8baf5ee2f4d5ed661142d98026b454462cdf4733
DiamondDat <diamond.dat@1pac.vn>
2017-04-19 16:38:36 +0700

    Add product to category seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ User.create!(name:  "Example User",
              admin: true,
              confirmed_at: Time.zone.now)
 
-99.times do |n|
+5.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -27,8 +27,9 @@ end
 
 # Products
 50.times do
-  name  = Faker::Commerce.product_name
-  price = Faker::Commerce.price
+  name   = Faker::Commerce.product_name
+  price  = Faker::Commerce.price
+  number = Faker::Number.between(1, 30)
   description = Faker::Lorem.paragraph(2, true)
   Product.create!(name:        name,
                   description: description,
@@ -37,6 +38,19 @@ end
 
 # Categories
 10.times do
-  name = Faker::Commerce.department
+  name = Faker::Commerce.department(1, true)
   Category.create!(name: name)
+end
+
+# Sub-Categories
+20.times do
+  name   = Faker::Commerce.department(1, true)
+  number = Faker::Number.between(1, 10)
+  Category.find(number).sub_categories.create!(name: name)
+end
+
+# Add product to category
+Product.all.each do |product|
+  number = Faker::Number.between(1, 30)
+  product.product_categories.create!(category_id: number)
 end
